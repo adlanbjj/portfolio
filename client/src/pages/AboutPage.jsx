01f8f7b01756eb828fe5a19a04e72c72a36c20c9
adlan isaev <adlanisaev@Air-de-adlan.lan>
2024-05-30 23:57:09 +0200

    projects page added

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,45 +1,63 @@
 import React from "react";
-import myImage from "../components/public/adlan.png";
+import myImage from "../public/adlan.png";
 import "./styles/Pages.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faInstagram,
   faLinkedin,
   faGithub,
-  faMessage,
 } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faDownload } from "@fortawesome/free-solid-svg-icons";
 
 const AboutPage = () => {
   return (
-      <div className="glob-cont">
-        <div className="glob-wrap-cont">
-          <div className="left-block">
-            <p>
-              Hello, i'm <span className="name-title">ADLAN ISAEV</span>
-            </p>
-            <p className="sec-title-text">
-              {" "}
-              Web and Mobile Applications Developer
-            </p>
-            <p className="desc-text">
-              As a passionate developer, I specialize in creating innovative and
-              user-friendly web and mobile applications. My focus is on
-              delivering high-quality and efficient software solutions
-            </p>
-            <div className="social-media-icons">
-              <FontAwesomeIcon className="contact-icon" icon={faInstagram} />
+    <div className="glob-cont">
+      <div className="glob-wrap-cont">
+        <div className="left-block">
+          <p>
+            Hello, I'm <span className="name-title">ADLAN ISAEV</span>
+          </p>
+          <p className="sec-title-text">
+            Web and Mobile Applications Developer
+          </p>
+          <p className="desc-text">
+            As a passionate developer, I specialize in creating innovative and
+            user-friendly web and mobile applications. My focus is on delivering
+            high-quality and efficient software solutions.
+          </p>
+          <div className="social-media-icons">
+            <a
+              href="https://www.instagram.com/adlan.str/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            <FontAwesomeIcon className="contact-icon" icon={faInstagram} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/adlan-isaev-1097a6227/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FontAwesomeIcon className="contact-icon" icon={faLinkedin} />
+            </a>
+            <a
+              href="https://github.com/adlanbjj/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FontAwesomeIcon className="contact-icon" icon={faGithub} />
-              <FontAwesomeIcon className="contact-icon" icon={faEnvelope} />
-            </div>
-          </div>
-          <div className="right-block">
-            <img src={myImage} alt="My Image" />
+            </a>{" "}
+            <FontAwesomeIcon className="contact-icon" icon={faEnvelope} />
+            <a href="/resume.pdf" download className="download-icon contact-icon">
+              <FontAwesomeIcon icon={faDownload} />
+            </a>
           </div>
         </div>
+        <div className="right-block">
+          <img src={myImage} alt="My Image" />
+        </div>
       </div>
+    </div>
   );
 };
-
 export default AboutPage;
--- client/src/pages/ProjectsPage.jsx
+++ client/src/pages/ProjectsPage.jsx
@@ -1,11 +1,69 @@
 import React from "react";
+import Cards from '../components/Cards/ProjectsCard/Cards';
 
-const ProjectsPage = () => {
+const ProjectsPage = ({ title, description, techStack, livePreviewLink, codeLink }) => {
+
+  const projects = [
+    {
+      title: 'Project title 1',
+      description: 'This is sample project description for project 1.',
+      techStack: 'HTML/CSS, JavaScript',
+      livePreviewLink: '#',
+      codeLink: '#'
+    },
+    {
+      title: 'Project title 2',
+      description: 'This is sample project description for project 2.',
+      techStack: 'React, Redux',
+      livePreviewLink: '#',
+      codeLink: '#'
+    },
+    {
+      title: 'Project title 3',
+      description: 'This is sample project description for project 3.',
+      techStack: 'Node.js, Express',
+      livePreviewLink: '#',
+      codeLink: '#'
+    },
+    {
+      title: 'Project title 4',
+      description: 'This is sample project description for project 4.',
+      techStack: 'Python, Django',
+      livePreviewLink: '#',
+      codeLink: '#'
+    },
+    {
+      title: 'Project title 5',
+      description: 'This is sample project description for project 5.',
+      techStack: 'Java, Spring Boot',
+      livePreviewLink: '#',
+      codeLink: '#'
+    },
+    {
+      title: 'Project title 6',
+      description: 'This is sample project description for project 6.',
+      techStack: 'PHP, Laravel',
+      livePreviewLink: '#',
+      codeLink: '#'
+    },
+  ];
+
   return (
     <div>
-      <h1>This is Projects page</h1>
+      <div className="card-container">
+      {projects.map((project, index) => (
+        <Cards
+          key={index}
+          title={project.title}
+          description={project.description}
+          techStack={project.techStack}
+          livePreviewLink={project.livePreviewLink}
+          codeLink={project.codeLink}
+        />
+      ))}
+    </div>
     </div>
   );
-};
+}
 
 export default ProjectsPage;
